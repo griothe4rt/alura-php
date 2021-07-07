16be7bc1393946cdeff88a977a661fce163853f4
Lucas Bomfim <lucascostabomfim5@gmail.com>
2021-07-07 00:36:13 -0300

    adicionando metodos

--- banco.php
+++ banco.php
@@ -2,10 +2,12 @@
 
 require_once 'src/Conta.php';
 
-$primeiraConta =  new Conta();
+$primeiraConta =  new Conta('123.456.789-10', 'Lucas Bomfim'); #Por conta do metodo Construct não consigo mais criar Conta sem passar os parametros
+var_dump($primeiraConta);
 $primeiraConta->deposita(500);
-$primeiraConta->saca(300); #essa forma está correta
+$primeiraConta->saca(100); #essa forma está correta
 # $primeiraConta->saldo -= 300; Porem, isso aqui dá um problema de segurança, deixando o saldo negativo
-$primeiraConta->defineCpfTitular('123.456.789-10');
 
-echo $primeiraConta->recuperaSaldo();
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;

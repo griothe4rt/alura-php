8a5aacded3cafd4c0787e74f1aa073ddc422f4ad
Lucas Bomfim <lucascostabomfim5@gmail.com>
2021-07-09 16:54:28 -0300

    refatorando codigo e estudando Hereditariedade

--- src/Titular.php
+++ src/Titular.php
@@ -1,35 +1,13 @@
 <?php
 
-class Titular
+class Titular extends Pessoa
 {
-    private CPF $CPF;
-    private string $nome;
     private Endereco $endereco;
 
     public function __construct(CPF $cpf, string $nome, Endereco $endereco)
     {
-        $this->validaNomeTitular($nome);
-        $this->CPF = $cpf;
-        $this->nome = $nome;
+        parent::__construct($nome, $cpf);
         $this->endereco = $endereco;
-    }
-
-    public function recuperaCpf(): string
-    {
-        return $this->CPF->recuperaNumero();
-    }
-
-    public function recuperaNome(): string
-    {
-        return $this->nome;
-    }
-
-    private function validaNomeTitular(string $nomeTitular)
-    {
-        if (strlen($nomeTitular) < 5) {
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        }
     }
 
     public function recuperaEndereco(): Endereco

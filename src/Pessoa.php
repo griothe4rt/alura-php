ba8b024236887112b73ae47d4da17a6a5d82ab86
Lucas Bomfim <lucascostabomfim5@gmail.com>
2021-07-09 16:54:02 -0300

    adicionando classe Pessoa

---
+++ src/Pessoa.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class Pessoa
+{
+    protected string $nome;
+    public CPF $CPF;
+
+    public function __construct(string $nome, CPF $cpf)
+    {
+        $this->validaNomeTitular($nome);
+        $this->nome = $nome;
+        $this->CPF = $cpf;
+    }
+
+    public function recuperaNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function recuperaCpf(): string
+    {
+        return $this->CPF->recuperaNumero();
+    }
+    protected function validaNomeTitular(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "Nome precisa ter pelo menos 5 caracteres";
+            exit();
+        }
+    }
+}
